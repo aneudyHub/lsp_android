2ab946337c06978b3c3e18c524ced226cc501d6c
ing.aneudy_v <29164616+aneudyvargas@users.noreply.github.com>
2018-02-22 14:48:39 -0400

    ok

--- app/src/main/java/com/system/lsp/utilidades/URL.java
+++ app/src/main/java/com/system/lsp/utilidades/URL.java
@@ -6,9 +6,9 @@ package com.system.lsp.utilidades;
 
 public class URL {
 
-    public final static String SERVER="http://api.inverjc.com/";
+    //public final static String SERVER="http://api.inverjc.com/";
     public final static String FOTO="http://www.inverjc.com/storage/archivos/clientes/";
-    //public final static String SERVER="http://192.168.10.77:8090/api.prestamos.com/v1/";
+    public final static String SERVER="http://192.168.10.77:8090/api.prestamos.com/v1/";
     //public final static String SERVER="http://192.168.0.5:8090/api.prestamos.com/v1/";
     public final static String HISTORIAL="cobrador/historial";
     public final static String LOGIN="usuarios/login";
