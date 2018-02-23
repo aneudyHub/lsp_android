9bd655ad4ff1cfc3efe2ee71195f33ff9c18d5d4
ing.aneudy_v <29164616+aneudyvargas@users.noreply.github.com>
2018-02-23 16:33:00 -0400

    point to public ip developer

--- app/src/main/java/com/system/lsp/utilidades/URL.java
+++ app/src/main/java/com/system/lsp/utilidades/URL.java
@@ -8,7 +8,7 @@ public class URL {
 
     //public final static String SERVER="http://api.inverjc.com/";
     public final static String FOTO="http://www.inverjc.com/storage/archivos/clientes/";
-    public final static String SERVER="http://192.168.10.77:8090/api.prestamos.com/v1/";
+    public final static String SERVER="http://143.137.83.145:8090/api.prestamos.com/v1/";
     //public final static String SERVER="http://192.168.0.5:8090/api.prestamos.com/v1/";
     public final static String HISTORIAL="cobrador/historial";
     public final static String LOGIN="usuarios/login";
